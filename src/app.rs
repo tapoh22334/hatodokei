bec13a9be76adf0a0d7eb0b3189ada1e53f322bb
iwase22334 <iwase22334@gmail.com>
2022-06-06 20:51:23 +0900

    update

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,3 @@
-use std::fs::File;
-use std::io::BufReader;
-
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TimeElement {
@@ -58,50 +55,186 @@ impl Default for Settings {
     }
 }
 
-//static VOICIES: (&[u8; 71956],
-//    &[u8; 64160], &[u8; 56992], &[u8; 66208], &[u8; 56992], &[u8; 56992],
-//    &[u8; 64160], &[u8; 70304], &[u8; 68256], &[u8; 59040], &[u8; 62112],
-//    &[u8; 82592], &[u8; 74400], &[u8; 79520], &[u8; 70304], &[u8; 73376],
-//    &[u8; 80544], &[u8; 82592], &[u8; 84640], &[u8; 74400], &[u8; 73376],
-//    &[u8; 88736], &[u8; 78496], &[u8; 90784]) = (
-//    include_bytes!("data/0000.wav"), include_bytes!("data/0100.wav"), include_bytes!("data/0200.wav"),
-//    include_bytes!("data/0300.wav"), include_bytes!("data/0400.wav"), include_bytes!("data/0500.wav"),
-//    include_bytes!("data/0600.wav"), include_bytes!("data/0700.wav"), include_bytes!("data/0800.wav"),
-//    include_bytes!("data/0900.wav"), include_bytes!("data/1000.wav"), include_bytes!("data/1100.wav"),
-//    include_bytes!("data/1200.wav"), include_bytes!("data/1300.wav"), include_bytes!("data/1400.wav"),
-//    include_bytes!("data/1500.wav"), include_bytes!("data/1600.wav"), include_bytes!("data/1700.wav"),
-//    include_bytes!("data/1800.wav"), include_bytes!("data/1900.wav"), include_bytes!("data/2000.wav"),
-//    include_bytes!("data/2100.wav"), include_bytes!("data/2200.wav"), include_bytes!("data/2300.wav"),
-//    );
+//pub struct Dispatcher {
+//}
+//
+//impl Default for Dispatcher {
+//    fn default() -> Self {
+//        Self { ..Default::default() }
+//    }
+//        //std::thread::spawn( move || {
+//        //    let mut last_frame: DateTime<Local> = Local::now();
+//
+//        //    loop {
+//        //        let now: DateTime<Local> = Local::now();
+//        //        let sleep_milliseconds = (1000. / MAX_FPS) as u32;
+//
+//        //        std::thread::sleep(std::time::Duration::new(0, sleep_milliseconds * 1000000));
+//        //        let sleep_milliseconds = (last_frame - now).num_milliseconds()
+//        //            - ((1000.0 / MAX_FPS) as i64);
+//
+//        //        if  sleep_milliseconds > 0 {
+//        //            std::thread::sleep(std::time::Duration::new(0, sleep_milliseconds as u32 * 1000000));
+//        //            last_frame = now;
+//        //        }
+//        //    }
+//        //});
+//
+//}
 
+pub struct PresetVoice {}
+
+impl PresetVoice {
+    pub fn voice_data(index: u32) -> Vec<u8> {
+            let voice_data = vec![
+                include_bytes!("data/0000.wav").to_vec(), include_bytes!("data/0100.wav").to_vec(),
+                include_bytes!("data/0200.wav").to_vec(), include_bytes!("data/0300.wav").to_vec(),
+                include_bytes!("data/0400.wav").to_vec(), include_bytes!("data/0500.wav").to_vec(),
+                include_bytes!("data/0600.wav").to_vec(), include_bytes!("data/0700.wav").to_vec(),
+                include_bytes!("data/0800.wav").to_vec(), include_bytes!("data/0900.wav").to_vec(),
+                include_bytes!("data/1000.wav").to_vec(), include_bytes!("data/1100.wav").to_vec(),
+                include_bytes!("data/1200.wav").to_vec(), include_bytes!("data/1300.wav").to_vec(),
+                include_bytes!("data/1400.wav").to_vec(), include_bytes!("data/1500.wav").to_vec(),
+                include_bytes!("data/1600.wav").to_vec(), include_bytes!("data/1700.wav").to_vec(),
+                include_bytes!("data/1800.wav").to_vec(), include_bytes!("data/1900.wav").to_vec(),
+                include_bytes!("data/2000.wav").to_vec(), include_bytes!("data/2100.wav").to_vec(),
+                include_bytes!("data/2200.wav").to_vec(), include_bytes!("data/2300.wav").to_vec(),
+            ];
+            voice_data[index as usize].clone()
+    }
+}
+
+pub enum SoundSource {
+    Popopopin(),
+    Silence(f32),
+    Voice(u32),
+    Path(String),
+}
+
+pub struct PlayInfo {
+    volume: u32,
+    sources: Vec<SoundSource>,
+}
+
+pub struct ExSink {
+    volume: u32,
+    sink: Sink,
+}
+
+pub struct SoundCoordinator {
+    master_volume: u32,
+    stream: OutputStream,
+    stream_handle: OutputStreamHandle,
+    exsinks: Vec<ExSink>,
+}
+
+impl Default for SoundCoordinator {
+    fn default() -> Self {
+        let (s, sh) = OutputStream::try_default().unwrap();
+
+        Self {
+            master_volume: 100,
+            stream: s,
+            stream_handle: sh,
+            exsinks: Vec::<ExSink>::default(),
+        }
+
+    }
+}
+
+use std::time::Duration;
+use rodio;
+use rodio::{OutputStream, OutputStreamHandle, Sink};
+use rodio::source::{SineWave, Source};
+
+impl SoundCoordinator {
+    pub fn play(&mut self, playinfo: PlayInfo) {
+        let PlayInfo {volume, sources} = playinfo;
+        let mut sink = Sink::try_new(&self.stream_handle).unwrap();
+
+        let mut exsink: ExSink = ExSink {volume: volume, sink: sink};
+
+        self.update_exsinks();
+        for source in sources {
+            match source {
+                SoundSource::Popopopin()        => {Self::play_popopopin(&mut exsink.sink);}
+                SoundSource::Silence(sec)       => {Self::play_none(&mut exsink.sink, sec);}
+                SoundSource::Voice(index) => { Self::play_preset_voice(&mut exsink.sink, index); }
+                SoundSource::Path(_path)         => {} // Not implemented
+            };
+
+        }
+
+        exsink.sink.set_volume(
+            Self::to_volume_magnification(self.master_volume, volume));
+        self.exsinks.push(exsink);
+    }
+
+    pub fn set_master_volume(&mut self, mv: u32) {
+        self.master_volume = mv;
+        for ExSink {volume, sink} in &self.exsinks {
+            sink.set_volume(Self::to_volume_magnification(mv, *volume));
+        }
+    }
+
+    fn update_exsinks(&mut self) {
+        while let Some(index) = self.exsinks.iter().position( |ExSink{volume: _, sink}|  sink.empty() ) {
+            self.exsinks.remove(index);
+        }
+    }
+
+    fn to_volume_magnification(master_volume: u32, volume: u32) -> f32 {
+        (master_volume as f32) / 100. *
+        (volume as f32) / 100.
+    }
+
+    fn play_popopopin(sink: &mut Sink) {
+        let popopopin = vec![
+            SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
+            SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
+            SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
+            SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
+            SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
+            SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
+            SineWave::new(880.0).take_duration(Duration::from_secs_f32(2.)).amplify(0.20),
+        ];
+        for s in popopopin {
+            sink.append(s);
+        }
+    }
+
+    fn play_none(sink: &mut Sink, sec: f32) {
+        let sinwave = SineWave::new(0.).take_duration(Duration::from_secs_f32(sec)).amplify(0.0);
+        sink.append(sinwave);
+    }
+
+    fn play_preset_voice(sink: &mut Sink, index: u32) {
+        let source = rodio::Decoder::new(
+            std::io::Cursor::new(PresetVoice::voice_data(index))).unwrap();
+
+        sink.append(source);
+    }
+}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     settings: Settings,
-
     #[serde(skip)]
-    voice_data: Vec<Vec<u8>>,
-    #[serde(skip)]
-    stream: Option<OutputStream>,
-    #[serde(skip)]
-    stream_handle: Option<OutputStreamHandle>,
-    #[serde(skip)]
-    sink: Option<Sink>,
-
+    sound_coordinator: SoundCoordinator,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             settings: Settings::default(),
-            voice_data: Vec::default(),
-            stream: None,
-            stream_handle: None,
-            sink: None,
+            sound_coordinator: SoundCoordinator::default(),
         }
     }
+}
+
+impl TemplateApp {
 }
 
 impl TemplateApp {
@@ -110,46 +243,18 @@ impl TemplateApp {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
-        let voice_data: Vec<Vec<u8>> = vec![
-            include_bytes!("data/0000.wav").to_vec(), include_bytes!("data/0100.wav").to_vec(),
-            include_bytes!("data/0200.wav").to_vec(), include_bytes!("data/0300.wav").to_vec(),
-            include_bytes!("data/0400.wav").to_vec(), include_bytes!("data/0500.wav").to_vec(),
-            include_bytes!("data/0600.wav").to_vec(), include_bytes!("data/0700.wav").to_vec(),
-            include_bytes!("data/0800.wav").to_vec(), include_bytes!("data/0900.wav").to_vec(),
-            include_bytes!("data/1000.wav").to_vec(), include_bytes!("data/1100.wav").to_vec(),
-            include_bytes!("data/1200.wav").to_vec(), include_bytes!("data/1300.wav").to_vec(),
-            include_bytes!("data/1400.wav").to_vec(), include_bytes!("data/1500.wav").to_vec(),
-            include_bytes!("data/1600.wav").to_vec(), include_bytes!("data/1700.wav").to_vec(),
-            include_bytes!("data/1800.wav").to_vec(), include_bytes!("data/1900.wav").to_vec(),
-            include_bytes!("data/2000.wav").to_vec(), include_bytes!("data/2100.wav").to_vec(),
-            include_bytes!("data/2200.wav").to_vec(), include_bytes!("data/2300.wav").to_vec(),
-        ];
-
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        let (s, sh) = OutputStream::try_default().unwrap();
-        let snk = Sink::try_new(&sh).unwrap();
-
         if let Some(storage) = cc.storage {
             let mut stored: Self = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-
-            stored.voice_data    = voice_data;
-            stored.stream        = Some(s);
-            stored.stream_handle = Some(sh);
-            stored.sink          = Some(snk);
-
             return stored;
         }
 
         TemplateApp {
-            voice_data:    voice_data,
-            stream:        Some(s),
-            stream_handle: Some(sh),
-            sink:          Some(snk),
-
             ..Default::default()
         }
     }
+
 }
 
 impl eframe::App for TemplateApp {
@@ -161,28 +266,14 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Settings { master_volume, master_mute, time_table} = &mut self.settings;
+        let Settings { master_volume, master_mute, time_table } = &mut self.settings;
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
 
-                    if ui.button("Play sound 3").clicked() {
-                        let popopopin = popopopin_generator();
-                        for s in popopopin {
-                           self.sink.as_ref().unwrap().append(s)
-                        }
-                    }
-
-                    else if ui.button("Play sound 5").clicked() {
-                       // Load a sound from a file, using a path relative to Cargo.toml
-                       let file = File::open("sound.mp3").unwrap();
-                       let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-                       self.sink.as_ref().unwrap().append(source);
-                    }
-
-                    else if ui.button("Quit").clicked() {
+                    if ui.button("Quit").clicked() {
                         frame.quit();
                     }
 
@@ -204,16 +295,11 @@ impl eframe::App for TemplateApp {
 
                     // Write device setting
                     {
-                        let current_volume = self.sink.as_ref().unwrap().volume();
-                        let volume_magnification = (*master_volume as f32) / 100.;
-
-                        if *master_mute && current_volume != 0.0 {
-                            self.sink.as_ref().unwrap().set_volume(0.0);
+                        if *master_mute {
+                            self.sound_coordinator.set_master_volume(0);
                         }
-
-                        if ! *master_mute
-                            && (volume_magnification - current_volume).abs() >= 0.01 {
-                            self.sink.as_ref().unwrap().set_volume(volume_magnification);
+                        else {
+                            self.sound_coordinator.set_master_volume(*master_volume);
                         }
                     }
 
@@ -238,7 +324,7 @@ impl eframe::App for TemplateApp {
                 });
             })
 
-            .body(|mut body| {
+            .body(|body| {
                 body.rows(20., time_table.len(), |row_index, mut row| {
                     row.col(|ui| {
                         ui.label(
@@ -256,33 +342,24 @@ impl eframe::App for TemplateApp {
                         ui.checkbox(&mut time_table[row_index].mute, "");
                     });
                     row.col(|ui| {
+                        let voice_num = time_table[row_index].h;
+
                         let icon = emojis::get_by_shortcode("arrow_forward").unwrap().as_str();
                         let b = ui.small_button(icon).clicked();
                         if b {
-                            let popopopin = popopopin_generator();
-                            for s in popopopin {
-                                self.sink.as_ref().unwrap().append(s)
-                            }
-
-                            self.sink.as_ref().unwrap().append(
-                                silence_generator(0.75)
-                                );
-
-                            //let name = format!("{0:>02}{1:>02}.wav",
-                            //                   time_table[row_index].h,
-                            //                   time_table[row_index].m,);
-                            //let file = File::open("data/".to_owned() + &name).unwrap();
-                            //let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-                            let index = time_table[row_index].h as usize;
-                            //let cursor = std::io::Cursor::new(&self.voice_data[index]);
-                            let source = rodio::Decoder::new(
-                                std::io::Cursor::new( self.voice_data[index].clone() )).unwrap();
-                            self.sink.as_ref().unwrap().append(source);
+                            let sources: Vec<SoundSource> = vec![
+                                        SoundSource::Popopopin(),
+                                        SoundSource::Silence(0.75),
+                                        SoundSource::Voice(voice_num),
+                                    ];
+                            self.sound_coordinator.play(PlayInfo {
+                                    volume: 100,
+                                    sources: sources,
+                            });
                         }
                     });
                 });
             });
-
 
         });
 
@@ -293,26 +370,4 @@ fn clock_emoji(row_index: usize) -> String {
     char::from_u32(0x1f550 + row_index as u32 % 24)
         .unwrap()
         .to_string()
-}
-
-use std::time::Duration;
-use rodio;
-use rodio::{OutputStream, OutputStreamHandle, Sink};
-use rodio::source::{SineWave, Source, Amplify, TakeDuration};
-
-fn popopopin_generator() -> std::vec::Vec<Amplify<TakeDuration<SineWave>>> {
-    let v = vec![
-        SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
-        SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
-        SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
-        SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
-        SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20),
-        SineWave::new(0.).take_duration(Duration::from_secs_f32(0.75)).amplify(0.20),
-        SineWave::new(880.0).take_duration(Duration::from_secs_f32(2.)).amplify(0.20),
-    ];
-    v
-}
-
-fn silence_generator(sec: f32) -> Amplify<TakeDuration<SineWave>> {
-    SineWave::new(0.).take_duration(Duration::from_secs_f32(sec)).amplify(0.0)
 }
