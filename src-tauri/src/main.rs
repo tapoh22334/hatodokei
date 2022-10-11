a0d26504c852ed28417809cb60842cd9185907cc
iwase22334 <iwase22334@gmail.com>
2022-10-11 18:17:47 +0900

    update version number

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -86,7 +86,7 @@ fn main() {
                     }
                     "About" => {
                         let window = app.get_window("main").unwrap();
-                        tauri::api::dialog::message(Some(&window), "Hatodokei", "鳩時計時報 v1.3.3");
+                        tauri::api::dialog::message(Some(&window), "Hatodokei", "鳩時計時報 v1.4.0");
                     }
                     _ => {}
                 }
